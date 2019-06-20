f825aa1c6af11a90f5c76ce5a6129be2f7de4938
evan de st jeor <evandsj@gmail.com>
2019-06-19 23:01:18 -0400

    created a weapon that spawns a projectile prefab that is capable of destroying enemies

---
+++ Grief/Assets/Scripts/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int health = 100;
+
+    // effect when enemy dies
+
+    //public GameObject deathEffect;
+
+    public void TakeDamage (int damage)
+    {
+        health -= damage;
+
+        if (health <= 0)
+        {
+            Die();
+        }
+    }
+
+    void Die()
+    {
+        // for death effect to trigger
+
+        //Instantiate(deathEffect, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+}
